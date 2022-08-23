bd3e2dd814c8cd369e984556a8ccd1e840508d02
Raafi Rahman <rafhay101@gmail.com>
2022-08-23 19:04:15 -0400

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,18 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
+a = 55
+b = 34
+
+z = 2*a + b
+
+print(z)
+
+f"""
 # Face Identification with Siamese Neural Networks!
 ## Made by Raafi Rahman
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
+answer = {z}
 """
-
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
