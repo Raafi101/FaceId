e097a1922d8da7298e6490efc426d61d52b17f6b
Raafi Rahman <rafhay101@gmail.com>
2022-08-30 12:08:55 -0400

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 print('Hello')
 
-from collections import namedtuple
 import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
 # Machine Learning
@@ -27,10 +24,10 @@ model = tf.keras.models.load_model('lessFP.h5', custom_objects={'contrastive_los
 
 dim = 128
 
-img1 = cv2.imread('Data/tests/raafi.jpg')
+img1 = cv2.imread('Data/raafi.jpg')
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 
-img2 = cv2.imread('Data/tests/kevin.jpg')
+img2 = cv2.imread('Data/kevin.jpg')
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 
 img1 = cv2.resize(img1, (dim, dim), interpolation = cv2.INTER_AREA)/255
@@ -47,6 +44,9 @@ z = 2*a + b
 
 print(z)
 
+print(pred)
+
+
 f"""
 # Face Identification with Siamese Neural Networks!
 ## Made by Raafi Rahman
@@ -54,7 +54,7 @@ answer = {z}
 prediction = {pred}
 """
 
-
+'''
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
@@ -75,3 +75,4 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+'''
