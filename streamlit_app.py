7f820a42c40c7b3474b85acc0117b4913b8e85bd
Raafi Rahman <rafhay101@gmail.com>
2022-08-30 12:14:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 print('Hello')
 
+from collections import namedtuple
 import altair as alt
 import streamlit as st
 
@@ -7,6 +8,8 @@ import streamlit as st
 import tensorflow as tf
 
 # Common Imports
+import pandas as pd
+import math
 import numpy as np
 import cv2
 
@@ -54,7 +57,7 @@ answer = {z}
 prediction = {pred}
 """
 
-'''
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
@@ -75,4 +78,3 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
-'''
